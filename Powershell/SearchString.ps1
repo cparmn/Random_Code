1b7d01a0e770cb0dcef2e82768c18629bec92b46
Casey Parman <parman.casey@gmail.com>
2018-11-29 09:31:49 -0600

    Create SearchString.ps1

---
+++ Powershell/SearchString.ps1
@@ -0,0 +1,42 @@
+<#
+.SYNOPSIS
+    Searches the filesystem for text files that contains passwords (potentially). The hits fit the criteria of at least four characters but no more than 15 
+    with one being an upper, lower, number, and special character.
+#>
+
+$dir ='C:\'
+
+# ============================================================================== 
+# Gets a listing of text files
+# ============================================================================== 
+$items = Get-ChildItem $dir -Recurse -Force -ErrorAction ignore | where {($_.extension -ne ".exe" -and $_.extension -ne ".dll")}
+
+# ============================================================================== 
+# Important Variables
+# ============================================================================== 
+$current_user = [Environment]::UserName 
+$newline = "`r`n"
+
+# ============================================================================== 
+# Loops through and searches for passwords
+# ============================================================================== 
+foreach($item in $items){
+$hits = Get-Content $item.fullname -ErrorAction ignore | Select-String -Pattern '^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).{4,15}$' | select-object line, linenumber
+$file_hit += $item.fullname + '+' +  ($hits -replace "@{line=",'' -replace "; linenumber=",'+' -replace "}",'') + $newline
+}
+
+# ============================================================================== 
+# Writes data to a file 
+# ============================================================================== 
+add-content -Path "c:\users\$current_user\desktop\hits.txt" -Value ($file_hit) 
+  
+# ============================================================================== 
+# Splits data into two columns and exports it as a csv 
+# ============================================================================== 
+import-csv "c:\users\$current_user\desktop\hits.txt" -Delimiter '+' -Header 'Path', 'Hits', 'LineNumber' |export-csv c:\users\$current_user\desktop\hits.xml 
+  
+# ============================================================================== 
+# Cleanup 
+# ============================================================================== 
+Remove-Item "c:\users\$current_user\desktop\hits.txt" 
+Remove-Variable file_hit, hits, item, items
